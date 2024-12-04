0ecb2d93f49308aa9c643573a5359bb8095e0993
Andrey <andrey051407@mail.ru>
2024-12-04 11:21:32 +0300

    Сохранены изменения в editor.xml

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrey/Desktop/AndreyM_CPP_HW
+# Build directory: /home/andrey/Desktop/AndreyM_CPP_HW/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external")

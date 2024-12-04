4bb026be4060b083d781df358dc13a393cabee5b
Andrey <andrey051407@mail.ru>
2024-12-04 13:03:55 +0300

    hw03 code

--- hw03/src/hw03.cpp
+++ hw03/src/hw03.cpp
@@ -1,6 +1,11 @@
 #include <hw03.h>
+#include <cstring>
 
 void hw03(char* str, const char* pattern)
 {
-  // Write code here..
+  const int len = strlen(str);
+  for (char itr = str[0]; itr != str.end(); itr++) {
+
+  }
+
 }

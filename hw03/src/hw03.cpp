e1a3668d6059f7bd319a365f7535bbfe1b186075
Andrey <andrey051407@mail.ru>
2024-12-04 14:52:43 +0300

    clear all

--- hw03/src/hw03.cpp
+++ hw03/src/hw03.cpp
@@ -5,22 +5,7 @@ using namespace std;
 
 void hw03(char* str, const char* pattern)
 {
-     const char* ptr_itr = pattern;
-     char* first_ch = str+strlen(ptr_itr);
-     char* last_ch = str+strlen(ptr_itr);
 
-     bool isEqviv = true;
-    for(char* str_itr = str; *str_itr != '\0'; str_itr++) {
-      for(int i = 0; ptr_itr[i] != '\0'; i++) {
-        if(*str_itr != *ptr_itr) {
-          isEqviv = false;
-          break;
-        }
-        if(isEqviv) {
-          memmove(str_itr, first_ch, strlen(first_ch+1));
-      }
-
-  }
 
 }
 }

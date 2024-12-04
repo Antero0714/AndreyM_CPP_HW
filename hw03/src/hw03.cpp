2558d8d2b112d1639e3d7f8c98241767832e3df5
Andrey <andrey051407@mail.ru>
2024-12-04 14:44:14 +0300

    hw03 code

--- hw03/src/hw03.cpp
+++ hw03/src/hw03.cpp
@@ -1,11 +1,26 @@
-#include <hw03.h>
+#include "/home/andrey/Desktop/AndreyM_CPP_HW/hw03/src/include/hw03.h"
 #include <cstring>
+#include <string>
+using namespace std;
 
 void hw03(char* str, const char* pattern)
 {
-  const int len = strlen(str);
-  for (char itr = str[0]; itr != str.end(); itr++) {
+     const char* ptr_itr = pattern;
+     char* first_ch = str+strlen(ptr_itr);
+     char* last_ch = str+strlen(ptr_itr);
+
+     bool isEqviv = true;
+    for(char* str_itr = str; *str_itr != '\0'; str_itr++) {
+      for(int i = 0; ptr_itr[i] != '\0'; i++) {
+        if(*str_itr != *ptr_itr) {
+          isEqviv = false;
+          break;
+        }
+        if(isEqviv) {
+          memmove(str_itr, first_ch, strlen(first_ch+1));
+      }
 
   }
 
+}
 }
